83178cb70bde92ca511215098083cb7bb49fd7cb
JlaraCast <163551527+JlaraCast@users.noreply.github.com>
2025-09-21 16:02:10 -0600

    jwt implementation into headquarter

--- src/routes/modules/headquarters/headquarter.routes.js
+++ src/routes/modules/headquarters/headquarter.routes.js
@@ -1,10 +1,22 @@
-const { HeadquarterController } = require('./headquarter.controller');
+const { HeadquarterController } = require('../../modules/headquarters/headquarter.controller');
+const { authenticate, authorize } = require('../../../middlewares/auth.middleware');
 
 module.exports = [
-  { method: 'GET', path: '/api/headquarters/active', handler: HeadquarterController.getAllActive },
-  { method: 'GET', path: '/api/headquarters', handler: HeadquarterController.getAll },
-  { method: 'GET', path: '/api/headquarters/:id', handler: HeadquarterController.getById },
-  { method: 'POST', path: '/api/headquarters', handler: HeadquarterController.create },
-  { method: 'PUT', path: '/api/headquarters/:id', handler: HeadquarterController.update },
-  { method: 'DELETE', path: '/api/headquarters/:id', handler: HeadquarterController.delete }
+
+  { method: 'GET', path: '/api/headquarters', handler: authenticate(HeadquarterController.getAll) },
+
+
+  { method: 'GET', path: '/api/headquarters/active', handler: authenticate(HeadquarterController.getAllActive) },
+
+
+  { method: 'GET', path: '/api/headquarters/:id', handler: authenticate(HeadquarterController.getById) },
+
+
+  { method: 'POST', path: '/api/headquarters', handler: authenticate(authorize('admin')(HeadquarterController.create)) },
+
+
+  { method: 'PUT', path: '/api/headquarters/:id', handler: authenticate(authorize('admin')(HeadquarterController.update)) },
+
+
+  { method: 'DELETE', path: '/api/headquarters/:id', handler: authenticate(authorize('admin')(HeadquarterController.delete)) },
 ];
