fd3c7cff3edb5132cc8bfd4b65e4121302fab22e
unknown <josueelmer1234@gmail.com>
2025-10-30 10:34:06 -0600

    Revert "Merge pull request #39 from stephvv23/MODULE-suppliers-Joan"
    This reverts commit a814574c006e44dc1c5d5ffa178a840c7f0069c4, reversing
    changes made to 0832fdf4a0f606efc08fb2b00b0a5513c4c326be.

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,7 +22,6 @@ const permissionRoutes = require('./modules/auth/permission.routes');
 const godParentRoutes = require('./modules/GodParent/godParent.routes');
 const activityRoutes = require('./modules/activity/activity.routes');
 const volunteerRoutes = require('./modules/volunteer/volunteer.routes');
-const suppliersRoutes = require('./modules/suppliers/suppliers.routes');
 
 // const patientsRoutes = require('./modules/patients.routes');
 // 2) Concatenate and compile paths → { method, pattern, paramNames, handler }
@@ -40,7 +39,6 @@ function buildRoutes() {
     ...godParentRoutes,
     ...activityRoutes,
     ...volunteerRoutes,
-    ...suppliersRoutes,
   ];
 
   // Sort by specificity ("/login" before "/:email")
